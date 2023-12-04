2d4280d1234fbeabecc10b4444b0c7ce13f8c1cd
ogaleasy <147885662+ogaleasy@users.noreply.github.com>
2023-12-03 19:24:32 -0500

    Delete S9-TAREA_3-GRUPO/src directory

--- S9-TAREA_3-GRUPO/src/js/MathExpressionFunctions/exercise3.js
+++
@@ -1,9 +0,0 @@
-function exercise3(value1, value2) {
-    let a = parseFloat(value1);
-    let b = parseFloat(value2);
-
-    let w = a - b + 2 * (a % b);
-    console.log("El valor de w es: " + w);
-    
-    return w;
-};
--- S9-TAREA_3-GRUPO/src/js/MathExpressionFunctions/exercise4.js
+++
@@ -1,9 +0,0 @@
-function exercise4(value1, value2) {
-    let a = parseFloat(value1);
-    let b = parseFloat(value2);
-
-    let v = 2 * b + Math.floor(a / 2) + 4 * (b % a);
-    console.log("El valor de v es: " + v);
-
-    return v;
-};
