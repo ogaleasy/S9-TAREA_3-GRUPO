3f8fd84123c451cbe017c2e1710871cbda58cd75
ogaleasy <147885662+ogaleasy@users.noreply.github.com>
2023-12-03 19:25:05 -0500

    Add files via upload

---
+++ src/js/MathExpressionFunctions/exercise3.js
@@ -0,0 +1,9 @@
+function exercise3(value1, value2) {
+    let a = parseFloat(value1);
+    let b = parseFloat(value2);
+
+    let w = a - b + 2 * (a % b);
+    console.log("El valor de w es: " + w);
+    
+    return w;
+};
