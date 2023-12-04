fdb02f12278011671a99771b58dad02fe14d4f8f
ogaleasy <147885662+ogaleasy@users.noreply.github.com>
2023-12-03 19:23:23 -0500

    Delete JavaScript-Project directory

--- JavaScript-Project/js/MathExpressionFunctions/exercise5.js
+++
@@ -1,9 +0,0 @@
-function exercise5(value1, value2) {
-    let a = parseFloat(value1);
-    let b = parseFloat(value2);
-
-    let u = b - a + 3 * (a % b);
-    console.log("El valor de u es: " + u);
-    
-    return u;
-};
